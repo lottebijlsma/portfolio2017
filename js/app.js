49c8a98d0c26d72551f4c1b0029819fc96eeee3c
lottebijlsma <bijlsmalotte@gmail.com>
2017-08-16 18:03:34 +0200

    :lipstick: Updates and shiz
    Updated css and style

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+// function onMouseMove(e) {
+//   console.log(e);
+//   Velocity(document.getElementById(`bg-anim`), {
+//     translateX: e.screenX,
+//     translateY: e.screenY
+//   }, {
+//     duration: 2000
+//   }
+//   );
+// }
+//
+//
+// function init() {
+//   window.addEventListener(`mousemove`, onMouseMove);
+// }
+//
+// init();
 
+// function init() {
+//   window.addEventListener(`mousemove`, onMouseMove);
+// }
+//
+// function onMouseMove(e) {
+//   const circle = document.createElement(`div`);
+//   circle.classList.add(`bg-anim`);
+//   document.querySelector(`.body`).appendChild(circle);
+//
+//   Velocity(document.getElementById(`bg-anim`), {
+//     translateX: e.pageX,
+//     translateY: e.pageY
+//   }
+//   );
+// }
